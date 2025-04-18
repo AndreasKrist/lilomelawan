4bb6980f48e18e611b6610a6addb448a8b8e87f0
Andreas Kristianto <andreaskrist2004@gmail.com>
2025-04-18 17:53:27 +0700

    Update ArticleContent.js

--- components/article/ArticleContent.js
+++ components/article/ArticleContent.js
@@ -1,6 +1,6 @@
 export default function ArticleContent({ children }) {
   return (
-    <div className="prose dark:prose-dark lg:prose-lg max-w-none mx-auto">
+    <div className="prose dark:prose-dark lg:prose-lg max-w-none mx-auto [&>p]:text-justify">
       {children}
     </div>
   );
