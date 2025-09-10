80d395ae8b4b44ec54cee622415959ed25e54c71
Andreas Kristianto <andreaskrist2004@gmail.com>
2025-09-10 16:18:59 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { ThemeProvider } from 'next-themes';
+import { Analytics } from '@vercel/analytics/next';
 import Layout from '../components/layout/Layout';
 import '../styles/globals.css';
 
@@ -8,6 +9,7 @@ function MyApp({ Component, pageProps }) {
       <Layout>
         <Component {...pageProps} />
       </Layout>
+      <Analytics />
     </ThemeProvider>
   );
 }
